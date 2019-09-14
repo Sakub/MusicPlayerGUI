b98d3e4cf75848ad99e7ebca7de10f8a2b2bb3e3
Jakub <sandomierzj@gmail.com>
2019-09-14 16:56:49 +0200

    Add list in which you can select song to play

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from tkinter import *
 import pygame
+import os
 root = Tk()
 root.title("Music Player by SakubDev")
 root.geometry("205x340")
 
 
-file = "./Music/dobryChlopak.mp3"
+os.chdir("./Music")
+print(os.getcwd)
+songList = os.listdir()
 
 
+playlist = Listbox(root, highlightcolor="blue", selectmode = SINGLE)
+print(songList)
+
+for item in songList:
+    pos = 0
+    playlist.insert(pos, item)
+    pos += 1
 
 def playSong():
-    print("Nothing special here")
     pygame.mixer.init()
-    pygame.mixer.music.load(file)
+    pygame.mixer.music.load(playlist.get(ACTIVE))
+    var.set(playlist.get(ACTIVE))
     pygame.mixer.music.play()
-
-    
 
 
 def stopSong():
-    print("Nothing special here")
     pygame.mixer.music.stop()
 
-
+var = StringVar()
+songTitle = Label(root, textvariable = var)
 
 PlayBtn = Button(root, width=5, height=3, text="Play", command=playSong)
 StopBtn = Button(root, width=5, height=3, text="Stop", command=stopSong)
-SongName = Label(root, text="Song Name")
 
 PlayBtn.pack(fill="x")
 StopBtn.pack(fill="x")
-SongName.pack(fill="both", expand="yes")
+songTitle.pack()
+playlist.pack(fill = "both", expand = "yes")
 
 root.mainloop()
